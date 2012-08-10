599ada7cf91d512f9f8563c5a25e863e8a43fabb
Oskar Thoren <o.thoren@gmail.com>
2012-08-10 12:42:42 -0400

    Run go fmt on lib.go

--- lib.go
+++ lib.go
@@ -1,27 +1,26 @@
-package ws-events
+package wsevents
 
 import (
-  "fmt"
-  "encoding/json"
 	"code.google.com/p/go.net/websocket"
+	"encoding/json"
+	"fmt"
 )
 
 type hub struct {
 	connections map[*connection]bool
-	broadcast chan event
-	register chan *connection
-	unregister chan *connection
+	broadcast   chan event
+	register    chan *connection
+	unregister  chan *connection
 }
 
-
 type connection struct {
-	ws *websocket.Conn
-  send chan event
+	ws   *websocket.Conn
+	send chan event
 }
 
 type event struct {
-  Name string `json:"eventName"`
-  Data interface{} `json:"data"` // takes arbitrary datas
+	Name string      `json:"eventName"`
+	Data interface{} `json:"data"` // takes arbitrary datas
 }
 
 var h = hub{
@@ -35,18 +34,18 @@ func (h *hub) run() {
 	for {
 		select {
 		case c := <-h.register:
-      fmt.Print("Register: ", h.connections)
+			fmt.Print("Register: ", h.connections)
 			h.connections[c] = true
 		case c := <-h.unregister:
 			delete(h.connections, c)
 			close(c.send)
 		case ev := <-h.broadcast:
-      fmt.Println("Broadcast: ", ev.Name, " : ", ev.Data)
+			fmt.Println("Broadcast: ", ev.Name, " : ", ev.Data)
 			for c := range h.connections {
 				select {
-        case c.send <- ev:
+				case c.send <- ev:
 				default:
-          delete(h.connections, c)
+					delete(h.connections, c)
 					close(c.send)
 					go c.ws.Close()
 				}
@@ -63,24 +62,24 @@ func (c *connection) reader() {
 			break
 		}
 
-    var ev event
-    if err := json.Unmarshal([]byte(message), &ev); err != nil {
-      fmt.Println(err)
-      break
-    }
-    h.broadcast <- ev
+		var ev event
+		if err := json.Unmarshal([]byte(message), &ev); err != nil {
+			fmt.Println(err)
+			break
+		}
+		h.broadcast <- ev
 	}
 	c.ws.Close()
 }
 
 func (c *connection) writer() {
 	for ev := range c.send {
-    msg, err := json.Marshal(ev)
-    if err != nil {
-      break
-    }
+		msg, err := json.Marshal(ev)
+		if err != nil {
+			break
+		}
 
-    if err := websocket.Message.Send(c.ws, string(msg)); err != nil {
+		if err := websocket.Message.Send(c.ws, string(msg)); err != nil {
 			break
 		}
 	}
@@ -88,7 +87,7 @@ func (c *connection) writer() {
 }
 
 func wsHandler(ws *websocket.Conn) {
-  fmt.Print("New websocket connection ", ws)
+	fmt.Print("New websocket connection ", ws)
 	c := &connection{send: make(chan event, 256), ws: ws}
 	h.register <- c
 	defer func() { h.unregister <- c }()
